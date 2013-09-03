ffc0317dcd3052a519808298028dac125f99ba5c
Sam Brown <sbrown2594@gmail.com>
2013-09-03 18:36:28 -0400

    Moving on to plugin functionality.

--- jquery-leadspend.js
+++ jquery-leadspend.js
@@ -1,7 +1,26 @@
 (function( $ ){
+	var leadspendAPI = "https://primary.api.leadspend.com/v2/validity/";
+	var timeout = 5;
+	
+	var jsonpValidateEmail = function() {
+		console.log("jsonpValidateEmail called");
+		/*if (emailAddress){
+			$.getJSON( leadspendAPI + encodeURIComponent( emailAddress ) + "?timeout=" + timeout + "&callback=?", null, {
+				emailAddress: emailAddress
+			}).done( function(data, textStatus, jqXHR) {
+				console.log( data );
+				console.log( textStatus );
+				console.log( jqXHR );
+				console.log( emailAddress );
+			}).fail(function( data ) {
+				console.log("fail");
+			});
+		}*/
+	};
+	
 	$.fn.leadSpendEmail = function() {
 		
-		this.on( "focusout", function(){alert("hello world");});
+		this.on( "focusout", jsonpValidateEmail() );
 
 		return this;
 	};
