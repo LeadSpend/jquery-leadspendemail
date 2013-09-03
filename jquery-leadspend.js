06f5ad2a4883cd827b5c8c943890723c0748bde0
Sam Brown <sbrown2594@gmail.com>
2013-09-03 18:32:13 -0400

    Figuring out why focusout isnt binding.

--- jquery-leadspend.js
+++ jquery-leadspend.js
@@ -1,27 +1,8 @@
 (function( $ ){
-	var leadspendAPI = "https://primary.api.leadspend.com/v2/validity/";
-	var timeout = 5;
-	
-	var jsonpValidateEmail = function() {
-		console.log("jsonpValidateEmail called");
-		/*if (emailAddress){
-			$.getJSON( leadspendAPI + encodeURIComponent( emailAddress ) + "?timeout=" + timeout + "&callback=?", null, {
-				emailAddress: emailAddress
-			}).done( function(data, textStatus, jqXHR) {
-				console.log( data );
-				console.log( textStatus );
-				console.log( jqXHR );
-				console.log( emailAddress );
-			}).fail(function( data ) {
-				console.log("fail");
-			});
-		}*/
-	}
-	
 	$.fn.leadSpendEmail = function() {
-		console.log("leadSpendEmail init");
+		//console.log("leadSpendEmail init");
 		
-		this.on( "focusout", function(){alert("hello world");});//jsonpValidateEmail() );
+		this.on( "focusout", function(){alert("hello world");});
 
 		return this;
 	};
