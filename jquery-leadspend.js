2c8635de270e0cccba0d4fa67ff8bf8a13041541
Sam Brown <sbrown2594@gmail.com>
2013-09-03 17:46:37 -0400

    Getting plugin running.

--- jquery-leadspend.js
+++ jquery-leadspend.js
@@ -2,22 +2,26 @@
 	var leadspendAPI = "https://primary.api.leadspend.com/v2/validity/";
 	var timeout = 5;
 	
-	var jsonpValidateEmail = function( emailAddress ) {	
-		$.getJSON( leadspendAPI + encodeURIComponent( emailAddress ) + "?timeout=" + timeout + "&callback=?", null, {
-			emailAddress: emailAddress
-		}).done( function(data, textStatus, jqXHR) {
-			console.log( data );
-			console.log( textStatus );
-			console.log( jqXHR );
-			console.log( emailAddress );
-		}).fail(function( data ) {
-			console.log("fail");
-		});
+	var jsonpValidateEmail = function( emailAddress ) {
+		if (emailAddress){
+			$.getJSON( leadspendAPI + encodeURIComponent( emailAddress ) + "?timeout=" + timeout + "&callback=?", null, {
+				emailAddress: emailAddress
+			}).done( function(data, textStatus, jqXHR) {
+				console.log( data );
+				console.log( textStatus );
+				console.log( jqXHR );
+				console.log( emailAddress );
+			}).fail(function( data ) {
+				console.log("fail");
+			});
+		}
 	}
 	
 	$.fn.leadSpendEmail = function() {
 		emailAddress = this.val();
-		this.on( "focusOut", jsonpValidateEmail( emailAddress ) );
+		
+		this.on( "focusout", jsonpValidateEmail( emailAddress ) );
+
 		return this;
 	};
 }( jQuery ));
