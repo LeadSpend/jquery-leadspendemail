59703a699d5ed619535a34198813b73d0313447d
Sam Brown <sbrown2594@gmail.com>
2013-09-05 18:01:46 -0400

    Working on hidden result form field.

--- jquery-leadspend.js
+++ jquery-leadspend.js
@@ -52,7 +52,7 @@
 		this.validateEmailInput = function(){
 			console.log( "validateEmailInput called" );
 			
-			if ( !$( $( this.element ).attr( "id" ) + "-result") ){// TODO: extend to support elements without IDs --> need UUID or global counter?
+			if ( $( $( this.element ).attr( "id" ) + "-result").length == 0 ){// TODO: extend to support elements without IDs --> need UUID or global counter?
 				$( this.element ).after( "<input class=\"leadSpendEmail-result\" id=\"" + $( this.element ).attr( "id" ) + "-result\">");
 			}
 			// CHECK FOR EXISTING .leadSpendEmail-result field with #EMAIL_FIELD_ID-RESULT ID
