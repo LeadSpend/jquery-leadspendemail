45932ce2bbc7696818ecd0c5405c7f9039de63ec
Sam Brown <sbrown2594@gmail.com>
2013-09-05 14:37:46 -0400

    Massively refactored to follow best practices.

--- jquery-leadspend.js
+++ jquery-leadspend.js
@@ -1,30 +1,57 @@
-(function( $ ){
-	var leadspendAPI = "https://primary.api.leadspend.com/v2/validity/";
-	var timeout = 5;
-	
-	var jsonpValidateEmail = function( emailInput ) {
-		console.log("jsonpValidateEmail called.  Email address val is:");
-		console.log( emailInput.val() )
-		emailAddress = emailInput.val();
-		
-		if (emailAddress){
-			$.getJSON( leadspendAPI + encodeURIComponent( emailAddress ) + "?timeout=" + timeout + "&callback=?", null, {
-				//emailAddress: emailAddress
-			}).done( function(data, textStatus, jqXHR) {
-				console.log( data );
-				console.log( emailAddress );
-				console.log( emailInput );
-			}).fail(function( data ) {
-				console.log("fail");
-			});
-		}
-	};
-	
-	$.fn.leadSpendEmail = function() {
-		var emailInput = this;
-		this.on( "focusout", function(){ jsonpValidateEmail( emailInput ); });//jsonpValidateEmail() );
+/*!
+ * LeadSpend Email Validation jQuery Plugin
+ * Original author: @this-sam, @leadspend
+ * Licensed under the MIT license
+ */
 
-		return this;
+;(function( $, window, document, undefined ){
+	//Initialize defaults
+	var pluginName = 'LeadSpendEmail',
+	defaults = {
+		leadspendApi: "https://primary.api.leadspend.com/v2/validity/",
+		timeout: 5
 	};
-}( jQuery ));
+	
+	function LeadSpendEmail( element, options){
+		this.element = element;
+		this.options = $.extend( {}, defaults, options)
+		this._defaults = defaults;
+        this._name = pluginName;
+		
+		this.jsonpValidateEmail = function() {
+			console.log("jsonpValidateEmail called.  Email address val is:");
+			console.log( this.element.val() )
+			emailAddress = this.element.val();
+			
+			if (emailAddress){
+				$.getJSON( leadspendAPI + encodeURIComponent( emailAddress ) + "?timeout=" + timeout + "&callback=?", null
+				).done( function(data, textStatus, jqXHR) {
+					console.log( data );
+					console.log( emailAddress );
+					console.log( this.element );
+				}).fail(function( data ) {
+					console.log("fail");
+				});
+			}
+		};
+        
+        this.init();
+	};
+
+	LeadSpendEmail.prototype.init = function () {
+		this.element.on( "focusout", function(){ jsonpValidateEmail(); });
+		return this.element;
+	};
+	
+	// A lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+	$.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, 
+                new Plugin( this, options ));
+            }
+        });
+    }
+}( jQuery, window, document ));
 
