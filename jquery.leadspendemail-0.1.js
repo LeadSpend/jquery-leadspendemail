ac361bc2ebcf3468ac039553be066952f835c856
Sam Brown <sbrown2594@gmail.com>
2013-09-12 16:48:36 -0400

    Still working on submit handling.

--- jquery.leadspendemail-0.1.js
+++ jquery.leadspendemail-0.1.js
@@ -207,6 +207,7 @@
 		if ( this.options.delaySubmit ){			
 			this.submitPressed = false;	// for tracking form submit
 			this.form = $( this.element ).parent( "form" ); // TODO: is this a strict enough selector?
+			console.log( this.form );
 		}
 		
 		$( this.element ).on( "focusout blur", $.proxy( this.validateEmailInput, this ) );  // TODO: also trigger on pressing enter	
