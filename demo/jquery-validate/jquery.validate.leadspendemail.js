1d65761b54736f557aabe15b23f1ea04d2a17f73
Sam Brown <sbrown2594@gmail.com>
2013-09-13 16:22:58 -0400

    Updating folder heirarchy

---
+++ demo/jquery-validate/jquery.validate.leadspendemail.js
@@ -0,0 +1,38 @@
+			// Set jQuery Validate to automatically ignore leadSpendEmail fields during onkeyup validation
+			(function( $ ){
+				jQuery.validator.addMethod(
+					"leadSpendEmail",
+					function( emailValue, element ) {	// actual validation function.  Return true if valid.
+						result = $( element ).siblings( ".leadSpendEmail-result" ).val();
+						switch ( result ){
+							case "illegitimate":
+							case "unreachable":
+							case "undeliverable":
+							case "pending":	// email result has not yet been returned.
+								return false;
+							
+							case "unknown":
+							case "disposable":
+							case "verified":
+							case "error": // only enabled in debug mode
+								return true;
+						}
+						return true; // always return a value, even if code is crazy
+					},
+					function( value, field ){	// function to determine error message
+						result = $( field ).siblings( ".leadSpendEmail-result" ).val();
+						switch ( result ){
+							case "illegitimate":
+							case "unreachable":
+							case "undeliverable":
+								return "Sorry, this email address appears to be " + result + ".";
+							
+							case "pending":
+								return "Verifying email address...";
+						}
+						return "";
+					} );
+				$( document ).ready( function(){
+					$( ".leadSpendEmail" ).closest( "form" ).validate();
+				});
+			}( jQuery ) );
