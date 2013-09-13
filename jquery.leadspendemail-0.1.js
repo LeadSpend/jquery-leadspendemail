8ee37b3404380a1f557f6bdc7a40f798364841ef
Sam Brown <sbrown2594@gmail.com>
2013-09-12 23:59:08 -0400

    General debugging and cleaning up.

--- jquery.leadspendemail-0.1.js
+++ jquery.leadspendemail-0.1.js
@@ -167,6 +167,8 @@
 			if ( this.submitPressed ){
 				console.log( "_handleDelaySubmit submitting form" )
 				this.submitPressed = false;
+				console.log( "\t $form is:" );
+				console.log( $( this.form ) );
 				// $( this.form ).off( "submit", $.proxy( this._submitHandler, this ) );	// unbind the specific function from the submit event
 				$( this.form ).children( "[type='submit']" ).click();
 			}
